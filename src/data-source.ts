78ea5f4bc680c97e149796d20c60d09ce26a2991
PedroItaloQ <peedroitalo2906@gmail.com>
2024-07-04 17:14:17 -0300

    refat: added loggin: true in data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -13,4 +13,5 @@ export const AppDataSource = new DataSource({
 	database: process.env.DB_NAME,
 	entities: [`${__dirname}/**/entities/*.{ts,js}`],
 	migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+	logging: true,
 })
